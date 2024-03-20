31adbb29354e5cfac64acb3e8cad35569eda56d7
qumiq <164170554+qumiq@users.noreply.github.com>
2024-03-20 19:06:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["shopping for sushi... ૮ ․ ․ ྀིა"];
 
 
 let currentIndex = 0;
